dbd3bcedceb9ad0aa8d4ea476418e4a238742be5
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-10-16 11:16:53 +0900

    内外区分マスタ追加

---
+++ Convenience/Models/DataModels/NaigaiClassMaster.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Convenience.Models.DataModels {
+
+    /// <summary>
+    /// 内外区分マスタＤＴＯ
+    /// </summary>
+    /// <Remarks>
+    /// 主キー：内外区分
+    /// </Remarks>
+    [Table("naigai_class_master")]
+    public class NaigaiClassMaster {
+
+        [Column("naigai_class")]
+        [DisplayName("内外区分")]
+        [Required]
+        [MaxLength(1)]
+        [Key]
+        public string NaigaiClass { get; set; }
+
+        [Column("naigai_class_name")]
+        [DisplayName("内外区分名称")]
+        [Required]
+        [MaxLength(30)]
+        public string NaigaiClassName { get; set; }
+        public virtual IEnumerable<KaikeiJisseki> KaikeiJissekis { get; set; }
+    }
+}
